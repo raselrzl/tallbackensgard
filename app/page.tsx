b996fb240f2590b092807d0f01fe8cf83351a48b
raselrzl <rasel6041@gmail.com>
2026-02-23 23:14:06 +0100

    shared homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         muted
         loop
         playsInline
-        className="absolute top-0 left-0 w-full h-225 object-cover -z-20"
+        className="absolute top-0 left-0 w-full h-225 object-cover -z-20 bg-black"
       >
         <source src="/homevideo.mp4" type="video/mp4" />
       </video>
