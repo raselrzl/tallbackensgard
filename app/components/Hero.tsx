7e607f063b24d0d2f104b73c2988575d943aa12d
raselrzl <rasel6041@gmail.com>
2026-02-24 12:38:28 +0100

    promo vedio test

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -64,9 +64,9 @@ const Hero: React.FC = () => {
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1 }}
-            className="px-6 py-3 min-w-46 uppercase rounded-lg text-white bg-gradient-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all tracking-widest font-[50]"
+            className="px-6 py-3 min-w-46 uppercase rounded-lg text-white bg-linear-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all tracking-widest font-[50]"
           >
-            Vandrarhem
+            Vandrarhem g
           </motion.button>
 
           <motion.button
