76b2bc911393e765cae5ed0dfaebc3f082ae40a7
raselrzl <rasel6041@gmail.com>
2026-02-24 15:07:31 +0100

    promo vedio test

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
 export default function HomePage() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
+    <div
+      className="relative w-screen h-screen overflow-hidden bg-cover bg-center"
+      style={{ backgroundImage: "url('/b1.jpg')" }} // fallback image
+    >
       {/* Logo - top center */}
       <Link
         href="/"
-        className="absolute top-2 left-1/2 transform -translate-x-1/2 z-20"
+        className="absolute top-5 left-1/2 transform -translate-x-1/2 z-20"
       >
         <Image
           src="/logo/logoc.png"
@@ -17,7 +20,7 @@ export default function HomePage() {
           width={400}
           height={400}
           priority
-          className="h-40 w-auto"
+          className="h-30 md:h-60 w-auto"
         />
       </Link>
 
@@ -25,6 +28,7 @@ export default function HomePage() {
       <video
         autoPlay
         muted
+        loop
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover"
       >
@@ -34,29 +38,28 @@ export default function HomePage() {
       {/* Overlay */}
       <div className="absolute top-0 left-0 w-full h-full bg-black/40" />
 
-      {/* Hero Content */}
+      {/* Hero Buttons at bottom center */}
       <section className="relative w-full h-screen flex items-end justify-center text-white text-center z-10 pb-12">
-      {/* Buttons at bottom center */}
-      <div className="flex flex-col sm:flex-row gap-4">
-        <motion.button
-          initial={{ opacity: 0, y: 40 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className="px-6 py-3 min-w-48 uppercase rounded-lg text-white bg-linear-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all tracking-widest font-julius"
-        >
-          Vandrarhem
-        </motion.button>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <motion.button
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="px-6 py-3 min-w-48 uppercase rounded-lg text-white bg-linear-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all tracking-widest font-julius"
+          >
+            Vandrarhem
+          </motion.button>
 
-        <motion.button
-          initial={{ opacity: 0, y: 40 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1 }}
-          className="px-6 py-3 min-w-48 uppercase rounded-lg text-white bg-linear-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all font-julius"
-        >
-          Apartments
-        </motion.button>
-      </div>
-    </section>
+          <motion.button
+            initial={{ opacity: 0, y: 40 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+            className="px-6 py-3 min-w-48 uppercase rounded-lg text-white bg-linear-to-r from-[#47d7ac] to-[#6ba4b8] hover:from-[#6ba4b8] hover:to-[#47d7ac] transition-all font-julius"
+          >
+            Apartments
+          </motion.button>
+        </div>
+      </section>
     </div>
   );
 }
