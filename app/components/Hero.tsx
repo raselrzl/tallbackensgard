44daa951564cdd0bb25291868fe852781af6c6e4
raselrzl <rasel6041@gmail.com>
2026-02-24 11:58:53 +0100

    promo vedio test

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,13 +6,13 @@ const Hero: React.FC = () => {
   return (
     <section className="relative w-full min-h-screen flex items-center justify-center text-white text-center">
       {/* Background Image */}
-    {/*   <div
+      {/*   <div
         className="absolute inset-0 bg-cover bg-center"
         style={{ backgroundImage: "url('/b1.jpg')" }}
       /> */}
 
-       {/* Background Video */}
-   {/*    <video
+      {/* Background Video */}
+      {/*    <video
         autoPlay
         muted
         loop
@@ -23,19 +23,19 @@ const Hero: React.FC = () => {
       </video> */}
 
       {/* Overlay */}
-     {/*  <div className="absolute inset-0 bg-black/20" /> */}
+      {/*  <div className="absolute inset-0 bg-black/20" /> */}
 
       {/* Content */}
       <div className="relative z-10 px-4 max-w-3xl mx-auto flex flex-col items-center">
         {/* Subtitle */}
-        <motion.p
+        {/*  <motion.p
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.6 }}
           className="text-xl md:text-2xl mb-2 font-bold"
         >
           Välkommen till
-        </motion.p>
+        </motion.p> */}
 
         {/* Title */}
         <motion.h1
@@ -45,20 +45,21 @@ const Hero: React.FC = () => {
           className="text-4xl md:text-6xl font-semibold leading-tight"
         >
           {/* Mobile */}
-          <span className="block md:hidden">
+          {/* <span className="block md:hidden">
             Tallbackens <br />
             Gård & <br />
             Vandrarhem
-          </span>
+          </span> */}
 
           {/* Desktop */}
-          <span className="hidden md:block whitespace-nowrap">
+          {/*  <span className="hidden md:block whitespace-nowrap">
             Tallbackens Gård & Vandrarhem
-          </span>
+          </span> */}
         </motion.h1>
 
         {/* Buttons */}
-        <div className="flex flex-col sm:flex-row gap-4 mt-8 items-center justify-center">
+        {/* Buttons */}
+        <div className="flex flex-col sm:flex-row gap-4 mt-140 mr-8 items-center justify-center">
           <motion.button
             initial={{ opacity: 0, y: 40 }}
             animate={{ opacity: 1, y: 0 }}
